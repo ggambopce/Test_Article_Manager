8d5b5f4c1976ac74f21d541e4b302f5c22838edc
ggambopce <ggambopce@gmail.com>
2023-06-16 16:57:59 +0900

    StudyStart

---
+++ Test_Article_Manager/src/main/java/com/koreaIT/Test_Article_Manager/TestArticleManagerApplication.java
@@ -0,0 +1,13 @@
+package com.koreaIT.Test_Article_Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestArticleManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestArticleManagerApplication.class, args);
+	}
+
+}
---
+++ Test_Article_Manager/src/test/java/com/koreaIT/Test_Article_Manager/TestArticleManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreaIT.Test_Article_Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestArticleManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

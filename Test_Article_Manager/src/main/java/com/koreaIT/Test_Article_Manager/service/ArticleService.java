0f12153a7f2fcd674d4319a0e1b0c7fee14d8c36
ggambopce <ggambopce@gmail.com>
2023-07-05 21:59:22 +0900

    Service구현

--- Test_Article_Manager/src/main/java/com/koreaIT/Test_Article_Manager/service/ArticleService.java
+++ Test_Article_Manager/src/main/java/com/koreaIT/Test_Article_Manager/service/ArticleService.java
@@ -3,31 +3,37 @@ package com.koreaIT.Test_Article_Manager.service;
 import java.util.List;
 
 import com.koreaIT.Test_Article_Manager.cotainer.Container;
+import com.koreaIT.Test_Article_Manager.dao.ArticleDao;
 import com.koreaIT.Test_Article_Manager.dto.Article;
 
 public class ArticleService {
 
+	private ArticleDao articleDao;
+
+	public ArticleService() {
+		this.articleDao = Container.articleDao;
+	}
 	public List<Article> getPrintArticles(String searchKeyword) {
-		return Container.articleDao.getPrintArticles(searchKeyword);
+		return articleDao.getPrintArticles(searchKeyword);
 	}
 
 	public int getLastId() {
-		return Container.articleDao.getLastId();
+		return articleDao.getLastId();
 	}
 
 	public void add(Article article) {
-		Container.articleDao.add(article);
+		articleDao.add(article);
 	}
 
 	public Article getArticleById(int id) {
-		return Container.articleDao.getArticleById(id);
+		return articleDao.getArticleById(id);
 	}
 
 	public void remove(Article foundArticle) {
-		Container.articleDao.remove(foundArticle);
+		articleDao.remove(foundArticle);
 	}
 
 	public void articleModify(Article foundArticle, String title, String body) {
-		Container.articleDao.articleModify(foundArticle, title, body);
+		articleDao.articleModify(foundArticle, title, body);
 	}
 }
--- Test_Article_Manager/src/main/java/com/koreaIT/Test_Article_Manager/service/MemberService.java
+++ Test_Article_Manager/src/main/java/com/koreaIT/Test_Article_Manager/service/MemberService.java
@@ -1,27 +1,34 @@
 package com.koreaIT.Test_Article_Manager.service;
 
 import com.koreaIT.Test_Article_Manager.cotainer.Container;
+import com.koreaIT.Test_Article_Manager.dao.MemberDao;
 import com.koreaIT.Test_Article_Manager.dto.Member;
 
 public class MemberService {
 
+	private MemberDao memberDao;
+
+	public MemberService() {
+		this.memberDao = Container.memberDao;
+	} 
+	
 	public int getLastId() {
-		return Container.memberDao.getLastId();
+		return memberDao.getLastId();
 	}
 
 	public void add(Member member) {
-		Container.memberDao.add(member);
+		memberDao.add(member);
 	}
 
 	public Member getMemberByLoginId(String loginId) {
-		return Container.memberDao.getMemberByLoginId(loginId);
+		return memberDao.getMemberByLoginId(loginId);
 	}
 
 	public boolean loginIdDupChk(String loginId) {
-		return Container.memberDao.loginIdDupChk(loginId);
+		return memberDao.loginIdDupChk(loginId);
 	}
 
 	public String getWriterName(int memberId) {
-		return Container.memberDao.getWriterName(memberId);
+		return memberDao.getWriterName(memberId);
 	}
 }
